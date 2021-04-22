4173882e8f025e8d4fdcf656842cfcb9c3356c75
Zakir Sajib <zakirsajib@yahoo.co.uk>
2021-04-23 01:41:34 +0600

    Update font-faces.js

--- packages/mars-theme/src/components/styles/font-faces.js
+++ packages/mars-theme/src/components/styles/font-faces.js
@@ -3,11 +3,15 @@ import DmRegular from "../../../fonts/dm-sans-v6-latin-regular.woff2";
 import DmMedium from "../../../fonts/dm-sans-v6-latin-500.woff2";
 import DmBold from "../../../fonts/dm-sans-v6-latin-700.woff2";
 
+import GeorgiaRegular from "../../../fonts/georgia_regular_font-webfont.woff2";
+import GeorgiaItalic from "../../../fonts/georgia_italic-webfont.woff2";
+import GeorgiaBold from "../../../fonts/georgia_bold-webfont.woff2";
+
 
 const fonts = {
-  "us-ascii": [DmRegular, DmMedium, DmBold],
-  latin: [DmRegular, DmMedium, DmBold],
-  all: [DmRegular, DmMedium, DmBold],
+  "us-ascii": [DmRegular, DmMedium, DmBold, GeorgiaRegular, GeorgiaItalic, GeorgiaBold],
+  latin: [DmRegular, DmMedium, DmBold, GeorgiaRegular, GeorgiaItalic, GeorgiaBold],
+  all: [DmRegular, DmMedium, DmBold, GeorgiaRegular, GeorgiaItalic, GeorgiaBold],
 };
 
 const FontFace = ({ state }) => {
@@ -39,63 +43,30 @@ const FontFace = ({ state }) => {
           font-display: "swap";
           src: url(${font[2]}) format("woff2");
         }
-
-        @font-face {
-            font-family: 'georgiabold_italic';
-            src: url('../../../fonts/georgia_bold_italic_font-webfont.woff2') format('woff2'),
-                 url('../../../fonts/georgia_bold_italic_font-webfont.woff') format('woff');
-            font-weight: normal;
-            font-style: normal;
-
-        }
-
-
-
-
-        @font-face {
-            font-family: 'georgiabold_italic';
-            src: url('../../../fonts/georgia_bold_italic-webfont.woff2') format('woff2'),
-                 url('../../../fonts/georgia_bold_italic-webfont.woff') format('woff');
-            font-weight: normal;
-            font-style: normal;
-
-        }
-
-
-
-
-        @font-face {
-            font-family: 'georgiabold';
-            src: url('../../../fonts/georgia_bold-webfont.woff2') format('woff2'),
-                 url('../../../fonts/georgia_bold-webfont.woff') format('woff');
-            font-weight: normal;
-            font-style: normal;
-
-        }
-
-
-
-
-        @font-face {
-            font-family: 'georgiaitalic';
-            src: url('../../../fonts/georgia_italic-webfont.woff2') format('woff2'),
-                 url('../../../fonts/georgia_italic-webfont.woff') format('woff');
-            font-weight: normal;
-            font-style: normal;
-
-        }
-
-
-
-
         @font-face {
             font-family: 'georgiaregular';
-            src: url('../../../fonts/georgia_regular_font-webfont.woff2') format('woff2'),
-                 url('../../../fonts/georgia_regular_font-webfont.woff') format('woff');
-            font-weight: normal;
+            src: url(${font[3]}) format("woff2");
+            font-weight: 400;
             font-style: normal;
 
         }
+        @font-face {
+            font-family: 'georgiaitalic';
+            src: url(${font[4]}) format("woff2");
+            font-weight: 400;
+            font-style: italic;
+
+        }
+        @font-face {
+            font-family: 'georgiabold';
+            src: url(${font[5]}) format("woff2");
+            font-weight: 700;
+            font-style: normal;
+        }
+
+
+
+
 
       `}
     />
