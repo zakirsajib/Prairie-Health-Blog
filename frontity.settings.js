41e29851c0b06e2bb504b0ac582627046a8947c1
Zakir Sajib <zakirsajib@yahoo.co.uk>
2021-04-25 07:49:45 +0600

    Update frontity.settings.js

--- frontity.settings.js
+++ frontity.settings.js
@@ -72,7 +72,7 @@ const settings = {
       "name": "@frontity/wp-source",
       "state": {
         "source": {
-          "url": "https://www.thefrontlash.com",
+          "url": "https://www.prairiehealth.co/blog",
           "params": {
             "per_page": 12,
             "type": ["post"],
